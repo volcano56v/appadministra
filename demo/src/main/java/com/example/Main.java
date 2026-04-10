96caabd2952a453a261da5393fc7721b548055f8
volcano56v <hayatolegendario@gmail.com>
2026-04-10 20:01:22 +0000

    se agrego una forma para hacer lo codigos de forma aleatoria

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,11 +1,13 @@
 package com.example;
 
+import java.util.random.RandomGenerator;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        Producto producto = new Producto(0, null, null);
-        Estante estante = new Estante(0, null);
-        Bodega bodega = new Bodega(0);
+        Producto producto = new Producto(generarNumerosAleatorios(), null, null);
+        Estante estante = new Estante(generarNumerosAleatorios(), null);
+        Bodega bodega = new Bodega(generarNumerosAleatorios());
 
         System.out.println("Este es Producto:");
         System.out.println(producto);
@@ -13,5 +15,14 @@ public class Main {
         System.out.println(estante);
         System.out.println("Este es Bodega:");
         System.out.println(bodega);
+    }
+
+    public static long generarNumerosAleatorios(){
+        long resultado;
+        long min = 100L;
+        long max = 1000L;
+        RandomGenerator generator = RandomGenerator.of("L64X128MixRandom");
+        resultado = generator.nextLong(min, max + 1);
+        return resultado;
     }
 }
